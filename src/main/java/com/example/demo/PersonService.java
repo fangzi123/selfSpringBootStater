84d7f972ed65d5712de4ea50bf6154b5393692a6
wangff <1024904798@qq.com>
2020-01-10 11:08:42 +0800

    自动配置

--- src/main/java/com/example/demo/PersonService.java
+++ src/main/java/com/example/demo/PersonService.java
@@ -1,26 +1,14 @@
 package com.example.demo;
- 
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PersonService {
-	
-	private PersonServiceProperties properties;
-	
-	public PersonService(PersonServiceProperties properties){
-		this.properties = properties;
-	}
-	
-	public PersonService(){
-		
-	}
-	
-	public String getPersonName(){
-		return properties.getName();
-	}
-	
-	public int getPersonAge(){
-		return properties.getAge();
-	}
-	
-	public String getPersonSex(){
-		return properties.getSex();
-	}
+
+    private String name;// 姓名
+    private int age;// 年龄
+    private String sex = "man";// 性别，不配置的时候默认为 man
+
 }
--- src/main/java/com/example/demo/PersonServiceAutoConfiguration.java
+++ src/main/java/com/example/demo/PersonServiceAutoConfiguration.java
@@ -1,25 +1,28 @@
 package com.example.demo;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
  
 @Configuration // 配置注解
 @EnableConfigurationProperties(PersonServiceProperties.class) // 开启指定类的配置
-@ConditionalOnClass(PersonService.class)// 当PersonService这个类在类路径中时，且当前容器中没有这个Bean的情况下，开始自动配置
+@ConditionalOnProperty(prefix = "person", name = "enable", havingValue = "true")//通过application.yml的person.enable=true|false 来控制Configuration是否生效
 public class PersonServiceAutoConfiguration {
+
 	@Autowired
 	private PersonServiceProperties properties;
 	
 	@Bean
   	@ConditionalOnMissingBean(PersonService.class)// 当容器中没有指定Bean的情况下，自动配置PersonService类
 	public PersonService personService(){
-		PersonService personService = new PersonService(properties);
+		PersonService personService = PersonService.builder()
+				.name(properties.getName())
+				.age(properties.getAge())
+				.sex(properties.getSex())
+				.build();
 		return personService;
 	}
 }
