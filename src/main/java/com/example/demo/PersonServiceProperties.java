ca7fc8561f39f03c5aa20f9bfddb33de08c87c3f
wangff <1024904798@qq.com>
2020-01-10 11:38:04 +0800

    自动配置

--- src/main/java/com/example/demo/PersonServiceProperties.java
+++ src/main/java/com/example/demo/PersonServiceProperties.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix="person")// 定义application.yml 配置文件中的配置前缀
 public class PersonServiceProperties {
-
+	private boolean enable;//控制Configuration是否生效
 	private String name;// 姓名
 	private int age;// 年龄
 	private String sex = "man";// 性别，不配置的时候默认为 man
